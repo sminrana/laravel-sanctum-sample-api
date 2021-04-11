9a3fe53e65c2b23bd3c8f5b789cdd8e023e45f2f
Smin Rana <sminrana@gmail.com>
2021-04-11 21:54:50 +0600

    Updated welcome page

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/', function () {
-    return redirect("https://www.sminrana.com");
+    return view('welcome');//redirect("https://www.sminrana.com");
 });
 
